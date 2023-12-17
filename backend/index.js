44d2d03ea5857e4c3c6ae246874615ee242a6c5d
Arvindh M <72850966+Arvindh177@users.noreply.github.com>
2023-12-17 09:57:14 +0530

    Update index.js
    Process.env

--- backend/index.js
+++ backend/index.js
@@ -3,42 +3,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config(); // To load environment variables from a .env file
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/mydatabase')
-.then((x)=>{
-    console.log(`Connected to Mongo Database name: "${x.connections[0].name}"`)
-})
-.catch((err)=> {
-    console.error('Error connect to mongo',err.reason)
-})
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log(`Connected to Mongo Database`);
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
 
-const employeeRoute = require('../backend/routes/employee.route')
+const employeeRoute = require('../backend/routes/employee.route');
 const app = express();
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: false,
-    }),
-)
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')))
-app.use('/',express.static(path.join(__dirname, 'dist/mean-stack-crud-app')))
-app.use('/api',employeeRoute);
+    })
+);
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/api', employeeRoute);
 
-const port = process.env.PORT || 4000
-const server = app.listen(port, ()=> {
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
     console.log('Connected to port ' + port);
-})
-
-app.use((req,res,next)=> {
-    next(createError(404))
 });
 
-app.use(function (err,req,res,next) {
-    console.error(err.message)
-    if(!err.statusCode) err.statusCode = 500
-    res.status(err.statusCode).send(err.message)
-})
+app.use((req, res, next) => {
+    next(createError(404));
+});
 
-
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
